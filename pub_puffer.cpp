8ff15f0e0a86bd587abab12990c4a95d8f8f80ed
Lion Waurich <lion.waurich@fhr.de>
2024-07-18 16:27:22 +0200

    serial kommunikation fehlerhaft

--- pub_puffer.cpp
+++ pub_puffer.cpp
@@ -1,14 +1,14 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 
-// SoftwareSerial Pins (10 und 11 können geändert werden)
+// SoftwareSerial Pins (10 und 11 können beliebig geändert werden)
 SoftwareSerial mySerial(10, 11); // RX, TX
 
 void setup() {
   // Öffne die Hardware-Serial-Kommunikation mit 115200 Baud für die Kommunikation mit dem PC
   Serial.begin(115200);
 
-  // Öffne die Software-Serial-Kommunikation mit 115200 Baud für die Kommunikation mit dem Raspberry Pi (Pins oben für TX/RX)
+  // Öffne die Software-Serial-Kommunikation mit 115200 Baud für die Kommunikation mit dem Raspberry Pi
   mySerial.begin(115200);
 
   // Warte darauf, dass die serielle Verbindung verfügbar wird
@@ -35,11 +35,14 @@ void loop() {
     if (input.equalsIgnoreCase("q")) {
       // Beenden der Schleife, wenn 'q' eingegeben wird
       Serial.println("Beenden des Programms...");
+      mySerial.println("q");
+      mySerial.flush(); // Ensure all data is sent
       while(true) {}
     }
 
     // Sende den eingegebenen Zustand an den Raspberry Pi
     mySerial.println(input);
+    mySerial.flush(); // Ensure all data is sent
 
     // Bestätige die Eingabe im Terminal
     Serial.println("Zustand gesendet: " + input);
